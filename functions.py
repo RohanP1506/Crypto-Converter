2c9d258dec63d737173ccf5164f7a14fe8b1ed1b
RohanP1506 <rohanapandit1506@gmail.com>
2023-02-04 20:13:15 -0600

    hi

---
+++ functions.py
@@ -0,0 +1,88 @@
+from pycoingecko import CoinGeckoAPI
+import datetime
+import time
+
+cg = CoinGeckoAPI()
+
+#list of ALL formatted dates, use for GRAPH
+def getFormattedDates(datelist):
+  formatted_list = []
+  for unix in datelist:
+    timestamp = datetime.datetime.fromtimestamp(unix)
+    dateUS = timestamp.strftime('%m-%d-%Y')
+    formatted_list.append(dateUS)
+  return formatted_list
+
+#per date format, used to correctly call data
+def formatDate(date):
+  timestamp = datetime.datetime.fromtimestamp(date)
+  dateUS = timestamp.strftime('%m-%d-%Y')
+  return dateUS
+
+#dates for unix conv, call data
+def getListDates(limit):
+  year = datetime.date.today().year - int(limit)
+  datelist = []
+  for num in range(1, int(limit) + 1):
+    month = 1
+    for num in range(1, 13):
+      date = datetime.date(year, num, 1)
+      year = int(date.year)
+      month = int(date.month)
+      day = int(date.day)
+      datelist.append(str(month) + '-' + str(day) + '-' + str(year))
+      month += 1
+    year += 1
+  return datelist
+
+#unix codes, call data
+def getUNIXDates(datelist):
+  unix_list = []
+  for date in datelist:
+    oneDate = datetime.datetime.strptime(date, '%m-%d-%Y')
+    unix = time.mktime(oneDate.timetuple())
+    unix_list.append(int(unix))
+  return unix_list
+
+# coin data
+def getCoinData(coin, years):
+  count = 0
+  finalList = {}
+  unix_list = getUNIXDates(getListDates(years))
+  coinJson = cg.get_coin_market_chart_range_by_id(
+    id=coin,
+    vs_currency='usd',
+    from_timestamp=str(unix_list[0]),
+    to_timestamp=str(unix_list[-1]))
+  path = coinJson['prices']
+  for list, date in zip(path, unix_list):
+    formatted = formatDate(date)
+    finalList[formatted] = "%.4f" % list[1]
+    count += 1
+  return finalList
+
+def combine(coin1, coin2, year):
+  
+  json1, json2 =getCoinData(coin1, year), getCoinData(coin2, year)
+  output={}
+  for key in json1.keys():
+    output[key] = [
+      json1[key],
+      json2[key]
+    ]
+  return output
+
+#name and ids
+def convertCoin(id):
+  coins = cg.get_coins_list()
+  for coin in coins:
+    if coin['name'] == id:
+      return coin['id']
+  return -1
+
+def getSupportedCoins():
+  supportedlist = []
+  x = cg.get_coins_list()
+  for name in x:
+    supportedlist.append(name['name'])
+  return supportedlist
---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, url_for, request
+import datetime
+from pycoingecko import CoinGeckoAPI
+from functions import *
+
+app = Flask('Crypto Comparer',
+            static_folder='static',
+            template_folder="templates")
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+  homedata = {
+    'supportedcoins': getSupportedCoins()
+  }
+  return render_template('index.html', data = homedata)
+
+
+@app.route('/results', methods=["GET", "POST"])
+def GraphGenerator():
+  coin1 = str(request.form.get('first'))
+  coin2 = str(request.form.get('second'))
+  years = request.form.get('third')
+  dates = []
+  firstmoney = []
+  secondmoney = []
+  for keys, values in getCoinData(convertCoin(coin1), years).items():
+    dates.append(keys)
+    firstmoney.append(values)
+  
+  for keys, values in getCoinData(convertCoin(coin2), years).items():
+    secondmoney.append(values)
+
+
+  graphdata = {
+    'firstCoin': getCoinData(convertCoin(coin1), years),
+    'secondCoin': getCoinData(convertCoin(coin2), years),
+    'firstmoney': firstmoney,
+    'dates': dates,
+    'secondmoney': secondmoney,
+    'coin1': coin1,
+    'coin2': coin2,
+    'combined': combine(convertCoin(coin1), convertCoin(coin2), years)
+
+  }
+
+  return render_template('results.html', data = graphdata)
+
+app.run(host='0.0.0.0', port=6969)
+
+
+
