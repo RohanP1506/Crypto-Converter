54c94454350a5a4cfbea2b82e02852d4a042c9ca
Jeydin21 <JeyPham21@gmail.com>
2023-02-15 15:30:37 -0600

    My last commit. For real this time

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from concurrent.futures import ThreadPoolExecutor
 from flask import Flask, render_template, request
 from functions import *
 
-app = Flask('Crypto Comparer',
+app = Flask('Crypto Converter',
             static_folder='static',
             template_folder="templates")
 
+
 @app.route('/', methods=["GET", "POST"])
 def home():
-  homedata = {
-    'supportedcoins': getSupportedCoins()
-  }
-  return render_template('index.html', data = homedata)
+  homedata = {'supportedcoins': getSupportedCoins()}
+  return render_template('index.html', data=homedata)
 
 
 @app.route('/results', methods=["GET", "POST"])
@@ -36,13 +35,13 @@ def GraphGenerator():
     'coin2': coin2,
     'combined': combine(convertCoin(coin1), convertCoin(coin2), years)
   }
-  return render_template('results.html', data = graphdata)
-
+                        
+  return render_template('results.html', data=graphdata)
 
 
 @app.route('/about', methods=['GET', 'POST'])
 def about():
   return render_template("about.html")
 
-app.run(host='0.0.0.0', port=6969)
 
+app.run(host='0.0.0.0', port=6969)
